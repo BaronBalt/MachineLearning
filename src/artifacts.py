ad306617c6c153a87d62b8e3f5465a159ec5fa5b
HippoLover123 <rebar.hamshin2003@gmail.com>
2026-02-04 18:09:20 +0100

    first DB table, docker-compose.yml to run it on all machines, made artifacts.py save to DB

--- src/artifacts.py
+++ src/artifacts.py
@@ -1,12 +1,45 @@
 import json
 import joblib
+import os
 from pathlib import Path
 from datetime import datetime, UTC
 
-def save_model(model, output_dir):
+try:
+    import psycopg
+except ImportError:
+    psycopg = None
+
+# The ML_DB_URL variable is used, the  local path is a fallback
+DB_URL = os.getenv("ML_DB_URL", "postgresql://mluser:mlpass@localhost:5432/mlregistry")
+
+def save_model_to_db(name, version, algorithm, artifact_path, accuracy):
+    with psycopg.connect(DB_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO model (name, version, algorithm, artifact_path, accuracy)
+                VALUES (%s, %s, %s, %s, %s)
+                    ON CONFLICT (name, version) DO NOTHING
+                """,
+                (name, version, algorithm, artifact_path, accuracy)
+            )
+
+
+def save_model(model, output_dir, *, model_name="random_forest", accuracy=None, version=None):
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     model_path = Path(output_dir) / "model.pkl"
     joblib.dump(model, model_path)
+    # only register to DB if we actually have accuracy
+    if accuracy is not None:
+        artifact_path = model_path.as_posix()
+        save_model_to_db(
+            name=model_name,
+            version=version,
+            algorithm=type(model).__name__,
+            artifact_path=artifact_path,
+            accuracy=accuracy,
+        )
+
     return model_path
 
 def save_metrics(metrics, output_dir):
