3a9c380622b693619526a9dc30b7b81483198293
Theodor Wase <twase@kth.se>
2026-02-12 20:10:26 +0100

    style: simplify env check

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import os
 from api.server import app
 
 if __name__ == "__main__":
-    debug = True
-    if env := os.getenv("APP_ENV", "development") == "production":
-        debug = False
+
+    debug = os.getenv("APP_ENV", "development") != "production"
 
     app.run(debug=debug, host="0.0.0.0", port=5000)
